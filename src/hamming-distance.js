6449b9530e0a650d91285f3a5e244fa6b3247c4b
Alfred Daimari <alfredwith@gmail.com>
2021-01-22 18:28:12 +0530

    added hamming-distance

---
+++ src/hamming-distance.js
@@ -0,0 +1,17 @@
+// function for applying hamming distance algorithm
+
+function ham_dis(string1, string2) {
+  if (string1.toString().length != string2.toString().length) {
+    throw new Error(string1 + "," + string2 + ": length not same");
+  }
+  count = 0;
+  for (let i in string1) {
+    if (string1[i] != string2[i]) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+module.exports = ham_dis;
