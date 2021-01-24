512e114261082eba4a839b4498fac9657ba1e77b
Alfred Daimari <alfredwith@gmail.com>
2021-01-25 01:13:44 +0530

    algo for longest-subsequence

---
+++ src/longest-subsequence.js
@@ -0,0 +1,24 @@
+// longest subsequence
+function long_sub(string1, string2) {
+  let arr = [];
+  for (let i = 0; i <= string2.length; i += 1) {
+    arr[i] = [0];
+  }
+  for (let j = 0; j <= string1.length; j += 1) {
+    arr[0][j] = 0;
+  }
+
+  for (let i = 1; i <= string2.length; i += 1) {
+    for (j = 1; j <= string1.length; j += 1) {
+      if (string2[i - 1] == string1[j - 1]) {
+        arr[i][j] = arr[i - 1][j - 1] + 1;
+      } else {
+        arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+      }
+    }
+  }
+
+  return arr[string2.length][string1.length];
+}
+
+module.exports = long_sub;
